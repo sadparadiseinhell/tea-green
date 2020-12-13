0e326e0416190ab954488d0fcb696f727317384a
sadparadiseinhell <atomica9999@gmail.com>
2020-12-13 19:44:11 +0200

    Removed the function of the weather updating by pressing the button
    Now the weather is updated automatically at a specified time interval

--- js/weather.js
+++ js/weather.js
@@ -2,58 +2,40 @@ const key = '';
 
 if (key == '') {
 	localStorage.removeItem('city');
+	document.querySelector("input[id='city']").style.display = 'none';
 	document.getElementById('remember').innerHTML = ('remember to add your api key!');
-	document.querySelector('.weather_update_button').style.display = 'none';
 } else {
 	document.getElementById('remember').style.display = 'none';
-}
-
-if (localStorage.getItem('city') == null) {
-	if (key == '') {
-		document.querySelector('input[id="city"]').style.display = 'none';
-	} else {
-		document.querySelector('.weather_update_button').style.display = 'none';
+	if (localStorage.getItem('city') == null) {
 		console.log(':(');
+	} else {
+		var city_name = localStorage.getItem('city');
+		getWeatherInfo(city_name);
+		document.getElementsByName('city_field')[0].placeholder = city_name;
 	}
-} else {
-	var city_name = localStorage.getItem('city');
-	getWeatherInfo(city_name);
-	document.getElementsByName('city_field')[0].placeholder = city_name;
-	console.log(':)');
 }
 
-function city() {
+var city_input = document.getElementById('city');
+
+city_input.addEventListener('keydown', function(){
 	if (event.keyCode == 13) {
-		let city_input = document.getElementById('city');
-		let city_name = city_input.value;
+		var city_name = city_input.value;
+		localStorage.setItem('city', city_name);
 		getWeatherInfo(city_name);
 
-		localStorage.setItem('city', city_name);
-		let city_ls = localStorage.getItem('city');
-		console.log(city_ls);
-		document.getElementsByName('city_field')[0].placeholder = city_ls;
+		document.getElementsByName('city_field')[0].placeholder = localStorage.getItem('city');
 		city_input.value = '';
 		city_input.blur();
-		document.querySelector('.weather_update_button').style.display = '';
+
+		console.log(localStorage.getItem('city'));
 	}
-}
-
-function update() {
-	const element = document.getElementById('spin')
-	element.classList.remove('spin');
-	void element.offsetWidth;
-	element.classList.add('spin');
-
-	var city_name = localStorage.getItem('city');
-	getWeatherInfo(city_name);
-	console.log('upd:', city_name);
-}
+});
 
 function getWeatherInfo(city_name) {
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${key}`)
 	.then(function (response) {
-		return response.json();
-	})
+            return response.json();
+        })
 	.then(function (data) {
 		celcius = Math.round(parseFloat(data.main.temp)-273.15);
 		fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
@@ -63,37 +45,47 @@ function getWeatherInfo(city_name) {
 	})
 	.then(function () {
 		drawWeather();
+	})
+	.catch(function (error) {
+		console.log(error);
 	});
+	setTimeout(function() {
+		getWeatherInfo(city_name)
+	}, 1800000);
 }
 
 function drawWeather() {
-	const today = new Date();
-	const hr = today.getHours();
-	if (hr >= 17 || hr < 6) {
+	var today = new Date();
+	var hour = today.getHours();
+
+	if (hour >= 17 || hour < 6) {
 		var time = 'night';
-		document.querySelector(":root").style.cssText = '--weather-bg: #303c7a';
 	} else {
 		var time = 'day';
 	}
 	
-	document.getElementById('description').innerHTML = description;
-	document.getElementById('icon').innerHTML = `<i id="icon" class="wi wi-owm-${time}-${id}"></i>`;
-
-	let temp = document.getElementById('temp');
-
-	if (localStorage.getItem('temp') === 'celcius') {
-		temp.innerHTML = celcius + '°C';
-	} else {
-		temp.innerHTML = fahrenheit + '°F';
+	if (time == 'night') {
+		document.querySelector(':root').style.cssText = '--weather-bg: #303c7a';
 	}
 
+	document.getElementById('description').innerHTML = `${description}`;
+	document.getElementById('icon').innerHTML = `<i id='icon' class='wi wi-owm-${time}-${id}'></i>`;
+
+	var temp = document.getElementById('temp');
+
 	temp.onclick = function() {
-		if (localStorage.getItem('temp') === 'celcius') {
+		if (localStorage.getItem('temp') == 'celcius') {
 			localStorage.setItem('temp', 'fahrenheit');
-			temp.innerHTML = fahrenheit + '°F';	
+			temp.innerHTML = `${fahrenheit}°F`;	
 		} else {
 			localStorage.setItem('temp', 'celcius');
-			temp.innerHTML = celcius + '°C';
+			temp.innerHTML = `${celcius}°C`;
 		}
-	}	
+	}
+
+	if (localStorage.getItem('temp') == 'celcius') {
+		temp.innerHTML =` ${celcius}°C`;
+	} else {
+		temp.innerHTML = `${fahrenheit}°F`;
+	}
 }
